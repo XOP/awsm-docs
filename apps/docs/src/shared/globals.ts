0e0634f8ea8889d0e998619eed34001c3ba5c6b6
XOP <wdybih@gmail.com>
2023-11-16 19:04:55 +0100

    docs layout and styling

---
+++ apps/docs/src/shared/globals.ts
@@ -0,0 +1,50 @@
+export const NAVIGATION_KEYS = {
+  core: 'core',
+  patterns: 'patterns',
+  components: 'components'
+} as const;
+
+export const NAVIGATION = [
+  {
+    slug: NAVIGATION_KEYS.core,
+    title: 'Core',
+  },
+  {
+    slug: NAVIGATION_KEYS.patterns,
+    title: 'Patterns',
+  },
+  {
+    slug: NAVIGATION_KEYS.components,
+    title: 'Components',
+  }
+];
+
+export const COMPONENT_NAVIGATION = [
+  {
+    slug: '',
+    title: 'Examples',
+  },
+  {
+    slug: 'usage',
+    title: 'Usage',
+  }
+];
+
+export const DEFAULT = 'default';
+
+export const EXAMPLE_SORT_KEYS = [
+  'base',
+  'color',
+  'size',
+  'variant',
+  'disabled',
+  'with'
+];
+
+export const LS_PREFIX = 'dsd';
+export const LS_KEY_META = 'meta';
+
+export const SIDEBAR_STATE = {
+  opened: 'opened',
+  closed: 'closed',
+} as const;
